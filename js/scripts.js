88e577bc2cb3854c32bc5962da1fe10bb6452c33
Brandan Sayarath and Jamison Cozart <jamison@>
2020-01-14 09:20:10 -0800

    Add text toggle animation clickable elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
-    $(".clickable").click(function() {
-      $("img").show();
-      $("#walrus-showing").slideToggle();
-      $("#walrus-hidden").slideToggle();
-    });
+  $(".clickable").click(function() {
+    $("img").show();
+    $("#walrus-showing").fadeToggle();
+    $("#walrus-hidden").fadeToggle();
   });
+  $(".clickable-p").click(function() {
+    $("#hidden-text").fadeToggle();
+  })
+});
